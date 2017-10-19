9ad02d6c6985fa7bb37f736f1390164b74d31e13
Unknown <snelson@senetco.com>
2017-10-19 16:56:05 -0400

    NAMote72 toolchain fil
    Enable ASM to compile board startup file from assembly format
    Adding nosys and nano spec resolves undefined references

--- loraMac-node/boards/namote72/namote72-toolchain.cmake
+++ loraMac-node/boards/namote72/namote72-toolchain.cmake
@@ -12,6 +12,10 @@ set(GCC_ARM_PATH "/opt/gcc/gcc-arm-none-eabi-5_4-2016q3/bin")
 CMAKE_FORCE_C_COMPILER(${GCC_ARM_PATH}/arm-none-eabi-gcc GNU)
 CMAKE_FORCE_CXX_COMPILER(${GCC_ARM_PATH}/arm-none-eabi-g++ GNU)
 
+SET(LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/cmsis/arm-gcc/stm32l152xc_flash.ld)
+SET(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs -Wl,--gc-sections -T ${LINKER_SCRIPT}")
+message(STATUS "Linker Flags= ${CMAKE_EXE_LINKER_FLAGS}")
+
 message(STATUS "Cross-compiling with the gcc-arm-embedded toolchain")
 
 # set(CMAKE_C_FLAGS
@@ -20,11 +24,22 @@ message(STATUS "Cross-compiling with the gcc-arm-embedded toolchain")
 #-Wl,--start-group -lgcc -lc -lm -lrdimon -Wl,--end-group")
 
 # -Wl,-gc-sections -lc")
+SET(COMMON_FLAGS "-mthumb -mcpu=cortex-m3 -ffunction-sections -fdata-sections")
+SET(CMAKE_C_FLAGS "${COMMON_FLAGS} -std=gnu99")
+SET(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -std=c++11")
+SET(CMAKE_ASM_FLAGS ${COMMON_FLAGS})
+# SET(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs -Wl,--gc-sections -T ${LINKER_SCRIPT}")
 
-  set(CMAKE_C_FLAGS
-  "-mthumb -mcpu=cortex-m3 -fno-builtin --specs=nosys.specs -std=c99 \
-  -ffunction-sections -fdata-sections -fomit-frame-pointer \
- -Wl,--start-group -mabi=aapcs -lc -lm -lrdimon -Wl,--end-group")
+# SET(LINKER_OPTIONS "-specs=nano.specs -Wl,--gc-sections")
+SET(LINKER_OPTIONS "-specs=nosys.specs -specs=nano.specs -Wl,--start-group -mabi=aapcs -lc -lm -lrdimon -Wl,--end-group")
+SET(CMAKE_EXE_LINKER_FLAGS "${LINKER_OPTIONS} -T ${LINKER_SCRIPT}")
+message(STATUS "Linker Flags: ${CMAKE_EXE_LINKER_FLAGS}")
+
+
+  #set(CMAKE_C_FLAGS
+  #"-mthumb -mcpu=cortex-m3 -fno-builtin --specs=nosys.specs -std=c99 \
+  #-ffunction-sections -fdata-sections -fomit-frame-pointer \
+ #-Wl,--start-group -mabi=aapcs -lc -lm -lrdimon -Wl,--end-group")
 
 
 # <Option name="UserEditLinker" value=" -mabi=aapcs; -lc; -lm; -lrdimon; ;"/>
