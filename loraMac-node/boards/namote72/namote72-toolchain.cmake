c385bfa82b67330856fb30b959b718d6c0e3aa10
Shaun Nelson <snelson@senetco.com>
2017-10-24 18:42:18 -0400

    classB and classC cmake builds
    classB and classC applications now build

--- loraMac-node/boards/namote72/namote72-toolchain.cmake
+++ loraMac-node/boards/namote72/namote72-toolchain.cmake
@@ -14,7 +14,6 @@ CMAKE_FORCE_CXX_COMPILER(${GCC_ARM_PATH}/arm-none-eabi-g++ GNU)
 
 SET(LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/cmsis/arm-gcc/stm32l152xc_flash.ld)
 SET(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs -Wl,--gc-sections -T ${LINKER_SCRIPT}")
-message(STATUS "Linker Flags= ${CMAKE_EXE_LINKER_FLAGS}")
 
 message(STATUS "Cross-compiling with the gcc-arm-embedded toolchain")
 
@@ -28,12 +27,12 @@ SET(COMMON_FLAGS "-mthumb -mcpu=cortex-m3 -ffunction-sections -fdata-sections")
 SET(CMAKE_C_FLAGS "${COMMON_FLAGS} -std=gnu99")
 SET(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -std=c++11")
 SET(CMAKE_ASM_FLAGS ${COMMON_FLAGS})
+SET(CMAKE_OBJCOPY ${GCC_ARM_PATH}/arm-none-eabi-objcopy)
 # SET(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs -Wl,--gc-sections -T ${LINKER_SCRIPT}")
 
 # SET(LINKER_OPTIONS "-specs=nano.specs -Wl,--gc-sections")
 SET(LINKER_OPTIONS "-specs=nosys.specs -specs=nano.specs -Wl,--start-group -mabi=aapcs -lc -lm -lrdimon -Wl,--end-group")
 SET(CMAKE_EXE_LINKER_FLAGS "${LINKER_OPTIONS} -T ${LINKER_SCRIPT}")
-message(STATUS "Linker Flags: ${CMAKE_EXE_LINKER_FLAGS}")
 
 
   #set(CMAKE_C_FLAGS
